3a6ede5d59f8f5491c0ef292b889bc79e21d49f0
Bui Tan Hung <staboyvn12@gmail.com>
2026-06-09 19:38:59 +0700

    Fixing Upload Document in Swagger UI

--- src/main/java/AiStudyHub/BE/dto/Request/DocumentUploadRequest.java
+++ src/main/java/AiStudyHub/BE/dto/Request/DocumentUploadRequest.java
@@ -1,6 +1,7 @@
 package AiStudyHub.BE.dto.Request;
 
 import AiStudyHub.BE.constraint.VisibilityStatus;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
@@ -15,12 +16,14 @@ import org.springframework.web.multipart.MultipartFile;
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class DocumentUploadRequest {
 
+    @Schema(type = "string", format = "binary", description = "Document file to upload")
     @NotNull(message = "FIELD_REQUIRED")
     MultipartFile file;
 
     @NotBlank(message = "FIELD_REQUIRED")
     String title;
 
+    @Schema(hidden = true)
     Long ownerId;
 
     @NotNull(message = "FIELD_REQUIRED")
--- src/main/java/AiStudyHub/BE/service/impl/IDocument.java
+++ src/main/java/AiStudyHub/BE/service/impl/IDocument.java
@@ -1,11 +1,9 @@
 package AiStudyHub.BE.service.impl;
 
 import AiStudyHub.BE.dto.Request.DocumentUploadRequest;
-import AiStudyHub.BE.entity.Document;
-import org.springframework.web.multipart.MultipartFile;
-
 import AiStudyHub.BE.dto.Response.DocumentUploadResponse;
 
 public interface IDocument {
-    DocumentUploadResponse uploadDocument(MultipartFile file, DocumentUploadRequest request) throws Exception;
+    DocumentUploadResponse uploadDocument(DocumentUploadRequest request) throws Exception;
 }
+
