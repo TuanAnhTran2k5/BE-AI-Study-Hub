658fe94ff57a91f890a09fa7acda2997daf4773c
Bui Tan Hung <staboyvn12@gmail.com>
2026-07-24 21:07:46 +0700

    fix: duplicate file java

--- src/main/java/AiStudyHub/BE/repository/ChatSessionDocumentRepo.java
+++ src/main/java/AiStudyHub/BE/repository/ChatSessionDocumentRepo.java
@@ -4,8 +4,12 @@ import AiStudyHub.BE.entity.ChatSessionDocument;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ChatSessionDocumentRepo extends JpaRepository<ChatSessionDocument, Long> {
-
+    List<ChatSessionDocument> findBySessionSessionId(Long sessionId);
+    long deleteBySessionSessionId(Long sessionId);
     long deleteByDocumentDocumentId(Long documentId);
 }
+
